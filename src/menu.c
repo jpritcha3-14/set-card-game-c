9045dfd149cc54ac521d48dedd168da236760022
Jonathan Pritchard <jpritcha3.14@gmail.com>
2021-08-23 20:24:47 -0700

    make cursor fancy

--- src/menu.c
+++ src/menu.c
@@ -3,7 +3,8 @@
 #include <ncurses.h>
 #include "structs.h"
 
-const char* CURSOR = "-->";
+const char* CURSOR_START = ">>>";
+const char* CURSOR_END = "<<<";
 
 const char *menu_option_text[] = {
   "Start",
@@ -14,16 +15,30 @@ const char *menu_option_text[] = {
 
 void draw_menu(WINDOW *menu_window) {
   int num_options = (int)(sizeof(menu_option_text)/sizeof(menu_option_text[0]));
+  int start;
   for (int i=0; i<num_options; i++) { 
-    mvwaddstr(menu_window, 2*i+1, LOGO_LETTER_W*3/2, menu_option_text[i]);
+    start = LOGO_LETTER_W*3/2 - strlen(menu_option_text[i])/2;
+    mvwaddstr(menu_window, 2*i+1, start, menu_option_text[i]);
   }
 }
 
 void draw_cursor(WINDOW *menu_window, int old, int new) {  
-  for (int i=0; i<LOGO_LETTER_W*3/2 - 1; i++) {
+  int old_start = LOGO_LETTER_W*3/2 - strlen(menu_option_text[old])/2;
+  int old_end = old_start + strlen(menu_option_text[old]);
+  int new_start = LOGO_LETTER_W*3/2 - strlen(menu_option_text[new])/2;
+  int new_end = new_start + strlen(menu_option_text[new]);
+  wattron(menu_window, COLOR_PAIR(WHITE));
+  mvwaddstr(menu_window, 2*old+1, old_start, menu_option_text[old]);
+  for (int i=old_start-6; i<old_start-1; i++) {
     mvwaddch(menu_window, 2*old+1, i, ' ');
   }
-    mvwaddstr(menu_window, 2*new+1, LOGO_LETTER_W*3/2 - strlen(CURSOR) - 1, CURSOR);
+  for (int i=old_end+1; i<old_end+6; i++) {
+    mvwaddch(menu_window, 2*old+1, i, ' ');
+  }
+  wattron(menu_window, COLOR_PAIR(new % 3 + 1));
+  mvwaddstr(menu_window, 2*new+1, new_start, menu_option_text[new]);
+  mvwaddstr(menu_window, 2*new+1, new_start - strlen(CURSOR_START) - 1, CURSOR_START);
+  mvwaddstr(menu_window, 2*new+1, new_end + 1, CURSOR_END);
 }
 
 Option show_menu(WINDOW *menu_window) {
